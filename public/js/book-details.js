3e3e73a2fd77c39b8918dee4d2d7cdec335ca6e0
elychnell <elychnell@gmail.com>
2026-09-10 12:46:20 +0200

    Fix moved imgs to public + review page forming

--- public/js/book-details.js
+++ public/js/book-details.js
@@ -15,22 +15,25 @@ Uppdatera befintlig bok med PATCH: http://localhost:3000/api/books/:id  (Token)
 Radera befintlig bok med DELETE: http://localhost:3000/api/books/:id  (Token)
 */
 
+
+
+function displayBookDetails() {
+
 const bookDetails = {
     title: "The Great Gatsby",
     description: "A novel set in the Roaring Twenties that tells the story of Jay Gatsby and his unrequited love for Daisy Buchanan.",
     author: "F. Scott Fitzgerald",
     genres: ["Fiction", "Classic"],
-    image: "the-great-gatsby.jpg",
+    image: "../images/large-pics/book1-large.jpg",
     published_year: 1925
 };
 
-function displayBookDetails() {
-    // TEST DATA bookId 
-    const bookId = "6aa13597dd2d1b7f961b2597"; 
-    // TEST DATA
 
-    const bookDetailsContainer = document.getElementById('book-details');
+    /* TEST DATA bookId 
+    //const bookId = "6aa13597dd2d1b7f961b2597"; 
+    
     const bookImgContainer = document.getElementById('bookImg');
+    const bookDetailsContainer = document.getElementById('book-details');
     try {
        fetch(`http://localhost:3000/api/books/${bookId}`)
             .then(response => response.json())
@@ -47,6 +50,19 @@ function displayBookDetails() {
     } catch (error) {
         console.error('Error displaying book details:', error);
     }
+    // TEST DATA  */
+
+    const bookImgContainer = document.getElementById('bookImg');
+    const bookDetailsContainer = document.getElementById('bookDetails');
+
+    bookImgContainer.innerHTML = `<img src="${bookDetails.image}" width="350" alt="${bookDetails.title}">`;
+    bookDetailsContainer.innerHTML = `
+        <h2>${bookDetails.title}</h2>
+        <p>${bookDetails.description}</p>
+        <p>Author: ${bookDetails.author}</p>
+        <p>Genres: ${bookDetails.genres.join(', ')}</p>
+        <p>Published Year: ${bookDetails.published_year}</p>
+    `;
 }
 
 displayBookDetails();
