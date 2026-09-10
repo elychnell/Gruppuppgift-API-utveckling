f1dd67d2a7a4a34acb0c82ff28245286086d0b88
Elena Holmberg <elenaholmberg83@hotmail.com>
2026-09-10 14:47:40 +0200

    feat(books): add populate and virtual

--- src/models/booksModel.ts
+++ src/models/booksModel.ts
@@ -1,31 +1,47 @@
 import mongoose from 'mongoose';
 const { Schema } = mongoose;
 
-const booksSchema = new Schema({
-  title: {
-    type: String,
-    required: true,
-  },
-  description: {
-    type: String,
-    required: true,
-  },
-  author: {
-    type: String,
-    required: true,
-  },
-  genres: {
-    type: Array,
-    required: true,
-  },
-  image: {
-    type: String,
-    required: true,
-  },
+const booksSchema = new Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+    author: {
+      type: String,
+      required: true,
+    },
+    genres: {
+      type: Array,
+      required: true,
+    },
+    image: {
+      type: String,
+      required: true,
+    },
 
-  published_year: {
-    type: Number,
-    required: true,
+    published_year: {
+      type: Number,
+      required: true,
+    },
   },
+  {
+    toJSON: {
+      virtuals: true,
+    },
+    toObject: {
+      virtuals: true,
+    },
+  },
+);
+
+booksSchema.virtual('reviews', {
+  ref: 'review', // The name of the model to populate
+  localField: '_id', // The field in the books collection that matches the foreign field
+  foreignField: 'book_id', // The field in the reviews collection that references the book
 });
 export default mongoose.model('book', booksSchema);
