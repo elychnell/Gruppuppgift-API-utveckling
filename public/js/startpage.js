a08a4dce2ab3b9fcbd3381ec9761b1e1de20bd71
elychnell <elychnell@gmail.com>
2026-09-15 20:59:28 +0200

    Re added changes to startpage.js

--- public/js/startpage.js
+++ public/js/startpage.js
@@ -17,19 +17,26 @@ async function getBooks() {
 function renderBooks(books, container) {
   const booksHTML = books
     .map(
-      (book) => `
-        <div class="book">
-          <a href="book-details.html?bookId=${book.id}">
-            <img src="${book.image}" alt="${book.title}" width="200">
-            <h3>${book.title}</h3>
-            <p>${book.description}</p>. 
-            <p>Author: ${book.author}</p>
-            <p>Genres: ${book.genres.join(', ')}</p>
-            <p>Published Year: ${book.published_year}</p>
-            
-          </a>
-        </div>
-      `,
+      (book) => {
+        const genresHTML = book.genres
+          .map(genre => `<span class="genre">${genre}</span>`)
+          .join('');
+
+        return `
+          <div class="book">
+            <a href="book-details.html?bookId=${book.id}">
+              <img src="${book.image}" alt="${book.title}" width="300">
+              <div class="bookInfo">
+                <h3>${book.title}</h3>
+                <p>${book.description}</p>
+                <div class="genres">
+                  ${genresHTML}
+                </div>
+              </div>
+            </a>
+          </div>
+        `;
+      },
     )
     .join('');
 
